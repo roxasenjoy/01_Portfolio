073b470a93c4aab84b87f089faf4a8a918902677
roxasenjoy <andrew.mahe14@gmail.com>
2020-11-13 18:01:06 +0100

    Contact php

---
+++ php/contact.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if(isset($_POST["submit"])){
+
+    /* Données */
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $sujet = $_POST['sujet'];
+    $message = $_POST['message'];
+
+    $mailTo = "andrew.mahe14@gmail.com";
+    $headers = "From: PORTFOLIO - " . $mailFrom;
+    $txt = "You are receive an e-mail from " .  $name . " | " . $telephone . " .\n\n" . $message;
+
+
+    mail($mailTo, $sujet, $txt, $headers);
+
+    header("Location:../main.html?mailsend");
+}
