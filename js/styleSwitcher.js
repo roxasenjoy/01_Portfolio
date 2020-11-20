ccd80ec5fde3da5f6f4b24205ac232fc27f5aa12
roxasenjoy <andrew.mahe14@gmail.com>
2020-11-20 04:05:27 +0100

    Color switcher

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -11,3 +11,22 @@ function setActiveStyle(color){
         }
     }
 }
+
+
+// Background
+    const bodySkin = document.querySelectorAll(".body-skin"),
+            totalBodySkin=bodySkin.length;
+
+    for(let i=0; i<totalBodySkin; i++){
+        bodySkin[i].addEventListener("change", function () {
+            if(this.value === "dark"){
+                document.body.className = "dark";
+            }else{
+                document.body.className = "";
+            }
+        })
+    }
+
+    document.querySelector(".toggle-style-switcher").addEventListener("click", () =>{
+        document.querySelector(".style-switcher").classList.toggle('open');
+    });
